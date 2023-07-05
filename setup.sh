6ce2ac04042e3b9807a048448df29616a1ad1aac
Harrison <harrison@pythonprogramming.net>
2023-07-05 14:06:47 -0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo pip3 install --upgrade torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+sudo pip3 install --upgrade huggingface-hub
+sudo pip3 install --upgrade transformers einops accelerate
+sudo pip3 install xformers
