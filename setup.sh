57213f66bb85b465f4a9489898953caf5ef573b2
Gojo1729 <shiva.s@thesilverlabs.com>
2023-07-06 19:12:54 +0530

    Reverted setup.sh changes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip3 install --upgrade torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip3 install --upgrade huggingface-hub
-pip3 install --upgrade transformers einops accelerate
-pip3 install xformers
+sudo pip3 install --upgrade torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+sudo pip3 install --upgrade huggingface-hub
+sudo pip3 install --upgrade transformers einops accelerate
+sudo pip3 install xformers
